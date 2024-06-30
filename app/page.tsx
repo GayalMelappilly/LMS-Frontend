3d301c5ce0ac4f80277445bb9a0cc70cb177c721
Gayal Melappilly <gayalsunil@gmail.com>
2024-07-01 02:51:08 +0530

    Added NavBar, ThemeSwitcher and SideBar in responsive design

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,30 @@
 'use client'
 import React, {FC, useState} from 'react'
+import Heading from './utils/Heading'
+import Header from './components/Header'
+import Hero from './components/Route/Hero'
 
 interface Props {}
 
 const Page:FC<Props> = () => {
+
+  const [open, setOpen] = useState(false)
+  const [activeItem, setActiveItem] = useState(0)
+
+
+  
   return (
     <div>
-      
+      <Heading 
+        title="eLearning"
+        description='eLearning is platform for students to learn and get help from teachers'
+        keywords='Programming, MERN, Redux, Machine Learning'
+      />
+      <Header
+        open={open}
+        setOpen={setOpen}
+        activeItem={activeItem}
+      />
     </div>
   )
 }
