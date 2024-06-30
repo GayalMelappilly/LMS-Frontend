a9420128d8b06d54b94cddfa192139e58d638aff
Gayal Melappilly <gayalsunil@gmail.com>
2024-07-01 01:28:31 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React, {FC, useState} from 'react'
+
+interface Props {}
+
+const Page:FC<Props> = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Page
