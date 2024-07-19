00fdff8c47e8f83228aaab3a185d1873708be135
Gayal Melappilly <gayalsunil@gmail.com>
2024-07-19 16:10:12 +0530

    Otp verification successful

--- app/components/Auth/Signup.tsx
+++ app/components/Auth/Signup.tsx
@@ -42,7 +42,7 @@ const Signup: FC<Props> = ({ setRoute }) => {
             password: ''
         },
         validationSchema: schema,
-        onSubmit: async ({ email, password }) => {
+        onSubmit: async ({ name, email, password }) => {
             const data = {
                 name, 
                 email,
