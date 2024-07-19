560f0b84f14061c0aff3203d05d30defa73b921e
Gayal Melappilly <gayalsunil@gmail.com>
2024-07-20 01:49:19 +0530

    Social Auth done, Google and Github

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import React, { FC, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import NavItems from '../utils/NavItems'
 import { ThemeSwitcher } from '../utils/ThemeSwitcher'
 import { HiOutlineMenuAlt3, HiOutlineUserCircle } from 'react-icons/hi'
@@ -10,6 +10,9 @@ import Verification from '../components/Auth/Verification'
 import { useSelector } from 'react-redux'
 import Image from 'next/image'
 import avatar from '../../public/assets/avatar.png'
+import { useSession } from 'next-auth/react'
+import { useSocialAuthMutation } from '@/redux/features/auth/authApi'
+import toast from 'react-hot-toast'
 
 type Props = {
     open: boolean,
@@ -24,6 +27,23 @@ const Header: FC<Props> = ({ activeItem, setOpen, route, open, setRoute }) => {
     const [active, setActive] = useState(false)
     const [openSidebar, setOpenSidebar] = useState(false)
     const { user } = useSelector((state: any) => state.auth)
+    const {data} = useSession()
+    const [socialAuth, {isSuccess, error}] = useSocialAuthMutation()
+
+    useEffect(()=>{
+        if(!user){
+            if(data){
+                socialAuth({
+                    email: data?.user?.email, 
+                    name: data?.user?.name, 
+                    avatar: data?.user?.image
+                })
+            }
+        }
+        if(isSuccess){
+            toast.success('Login successful')
+        }
+    }, [data, user])
 
     if (typeof window !== 'undefined') {
         window.addEventListener('scroll', () => {
--- redux/features/auth/authApi.ts
+++ redux/features/auth/authApi.ts
@@ -63,8 +63,33 @@ export const authApi = apiSlice.injectEndpoints({
                     console.log(error)
                 }
             }
+        }),
+        socialAuth: builder.mutation({
+            query: ({email, name, avatar}) => ({
+                url: 'social-auth',
+                method: "POST",
+                body: {
+                    email,
+                    name,
+                    avatar
+                },
+                credentials: "include" as const
+            }),
+            async onQueryStarted(arg, {queryFulfilled, dispatch}){
+                try{
+                    const result = await queryFulfilled
+                    dispatch(
+                        userLoggedIn({
+                            accessToken: result.data.accessToken,
+                            user: result.data.user  
+                        })
+                    )
+                }catch(error: any){
+                    console.log(error)
+                }
+            }
         })
     })
 })
 
-export const {useRegisterMutation, useActivationMutation, useLoginMutation} = authApi
+export const {useRegisterMutation, useActivationMutation, useLoginMutation, useSocialAuthMutation} = authApi
