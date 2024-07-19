f62aa65fbe53889719686900c5568f9a3dae2607
Gayal Melappilly <gayalsunil@gmail.com>
2024-07-20 02:02:54 +0530

    Added loader

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,8 @@ import { ThemeProvider } from "./utils/theme-provider";
 import { Toaster } from "react-hot-toast";
 import { Providers } from './Provider'
 import { SessionProvider } from "next-auth/react";
+import { useLoadUserQuery } from "@/redux/features/api/apiSlice";
+import Loader from './components/Loader/Loader'
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -31,7 +33,7 @@ export default function RootLayout({
         <Providers>
           <SessionProvider>
             <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-              {children}
+              <Custom>{ children }</Custom>
               <Toaster position="top-center" reverseOrder={false} />
             </ThemeProvider>
           </SessionProvider>
@@ -39,4 +41,15 @@ export default function RootLayout({
       </body>
     </html>
   );
+}
+
+const Custom: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const { isLoading } = useLoadUserQuery({})
+  return (
+    <>
+    {
+      isLoading ? <Loader /> : <>{children}</>
+    }
+    </>
+  )
 }
