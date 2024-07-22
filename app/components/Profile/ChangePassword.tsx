df1a2e31c9f767b6a0d6396f8e81bfa819abcacd
Gayal Melappilly <gayalsunil@gmail.com>
2024-07-23 01:02:29 +0530

    Change password, done

--- app/components/Profile/ChangePassword.tsx
+++ app/components/Profile/ChangePassword.tsx
@@ -1,5 +1,7 @@
 import { styles } from '@/app/styles/style'
-import React, { useState } from 'react'
+import { useUpdatePasswordMutation } from '@/redux/features/user/userApi'
+import React, { use, useEffect, useState } from 'react'
+import toast from 'react-hot-toast'
 
 type Props = {}
 
@@ -9,9 +11,27 @@ const ChangePassword = (props: Props) => {
     const [newPassword, setNewPassword] = useState('')  
     const [confirmPassword, setConfirmPassword] = useState('')
 
-    const passwordChangeHandler = (e: any) => {
-        
+    const [updatePassword, {isSuccess, error}] = useUpdatePasswordMutation()
+
+    const passwordChangeHandler = async (e: any) => {
+        e.preventDefault()
+        if(newPassword !== confirmPassword){
+            toast.error('Password does not match')
+        }else{
+            await updatePassword({oldPassword, newPassword})
+        }
     }
+
+    useEffect(() => {
+        if(isSuccess){
+            toast.success('Password Updated Successfully')
+        }
+        if(error){
+            const errorData = error as any
+            toast.error(errorData.data.message)
+        }
+    }, [isSuccess, error])
+    
 
   return (
     <div className="w-full pl-7 px-2 800px:px-5 800px:pl-0">
@@ -25,7 +45,7 @@ const ChangePassword = (props: Props) => {
                 className='flex flex-col items-center'
             >
                 <div className='w-[100%] 800px:w-[60%] mt-5'>
-                    <label className='block pb-2 text-black dark:text-[#fff]'>Enter your old password</label>
+                    <label className='block pb-2 text-black dark:text-[#fff]'>Old Password</label>
                     <input
                         type='password'
                         className={`${styles.input} !w-[95%] mb-4 800px:mb-0 text-black dark:text-[#fff]`}
@@ -35,7 +55,7 @@ const ChangePassword = (props: Props) => {
                     />
                 </div>
                 <div className='w-[100%] 800px:w-[60%] mt-5'>
-                    <label className='block pb-2 text-black dark:text-[#fff]'>Enter your new password</label>
+                    <label className='block pb-2 text-black dark:text-[#fff]'>New Password</label>
                     <input
                         type='password'
                         className={`${styles.input} !w-[95%] mb-4 800px:mb-0 text-black dark:text-[#fff]`}
