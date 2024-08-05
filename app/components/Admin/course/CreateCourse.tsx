f99ecc7a87907a1902b1e6deb99c2e49d132d4f7
Gayal Melappilly <gayalsunil@gmail.com>
2024-08-06 02:08:37 +0530

    Course section added

---
+++ app/components/Admin/course/CreateCourse.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import React, { useState } from 'react'
+import CourseInformation from './CourseInformation'
+import CourseOptions from './CourseOptions'
+
+type Props = {}
+
+const CreateCourse = (props: Props) => {
+
+    const [active, setActive] = useState(0);
+    const [courseInfo, setCourseInfo] = useState({
+        name: "",
+        description: "",
+        price: "",
+        estimatedPrice: "",
+        tags: "",
+        level: "",
+        categories: "",
+        demoUrl: "",
+        thumbnail: "",
+    });
+    const [benefits, setBenefits] = useState([{ title: "" }]);
+    const [prerequisites, setPrerequisites] = useState([{ title: "" }]);
+    const [courseContentData, setCourseContentData] = useState([
+        {
+            videoUrl: "",
+            title: "",
+            description: "",
+            videoSection: "Untitled Section",
+            videoLength: "",
+            links: [
+                {
+                    title: "",
+                    url: "",
+                },
+            ],
+            suggestion: "",
+        },
+    ]);
+
+    const [courseData, setCourseData] = useState({})
+
+    return (
+        <div className='w-full flex min-h-screen'>
+            <div className='w-[80%]'>
+                {
+                    active === 0 && (
+                        <CourseInformation />
+                    )
+                }
+            </div>
+            <div className="w-[20%] mt-[100px] h-screen fixed z-[-1] top-18 right-0">
+                <CourseOptions active={active} setActive={setActive} />
+            </div>
+        </div>
+    )
+}
+
+export default CreateCourse
