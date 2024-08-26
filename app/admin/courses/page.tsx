5b488d0a75f5923df85732253183d7c0ebb73da1
Gayal Melappilly <gayalsunil@gmail.com>
2024-08-26 22:04:24 +0530

    Get all courses data added, successfully

--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -1,35 +1,35 @@
 'use client'
 
-import React from 'react'
-import AdminSidebar from '../../components/Admin/sidebar/AdminSidebar'
-import Heading from '../../utils/Heading'
-import CreateCourse from '../../components/Admin/course/CreateCourse'
 import DashboardHeader from '@/app/components/Admin/DashboardHeader'
 import AdminProtected from '@/app/hooks/adminProtected'
+import Heading from '@/app/utils/Heading'
+import React from 'react'
+import AdminSidebar from '../../components/Admin/sidebar/AdminSidebar'
+import AllCourses from '../../components/Admin/course/AllCourses.tsx'
 
 type Props = {}
 
 const page = (props: Props) => {
-    return (
-        <div>
-            <AdminProtected>
+  return (
+    <div>
+        <AdminProtected>
                 <Heading
                     title='Learnify - Admin'
                     description='Learnify is a platform for students to learn and get help from teachers'
                     keywords='Programming,MERN,Redux,Machine Learning'
                 />
-                <div className='flex'>
+                <div className='flex h-screen'>
                     <div className='1500px:w-[16%] w-1/5'>
                         <AdminSidebar />
                     </div>
                     <div className='w-[85%]'>
                         <DashboardHeader />
-                        <CreateCourse />
+                        <AllCourses />
                     </div>
                 </div>
             </AdminProtected>
-        </div>
-    )
+    </div>
+  )
 }
 
 export default page
