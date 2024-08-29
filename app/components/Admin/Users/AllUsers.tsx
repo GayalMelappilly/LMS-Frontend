0fe7db6109d9b2891a666e25c52f11a09152e625
Gayal Melappilly <gayalsunil@gmail.com>
2024-08-29 23:35:15 +0530

    Update and delete user role added

--- app/components/Admin/Users/AllUsers.tsx
+++ app/components/Admin/Users/AllUsers.tsx
@@ -6,7 +6,9 @@ import { useTheme } from "next-themes";
 import Loader from "../../Loader/Loader";
 import { format } from "timeago.js";
 import {
+  useDeleteUserMutation,
   useGetAllUsersQuery,
+  useUpdateUserRoleMutation,
 } from "@/redux/features/user/userApi";
 import { styles } from "@/app/styles/style";
 import { toast } from "react-hot-toast";
@@ -22,12 +24,40 @@ const AllUsers: FC<Props> = ({ isTeam }) => {
   const [role, setRole] = useState("admin");
   const [open, setOpen] = useState(false);
   const [userId, setUserId] = useState("");
-
+  const [updateUserRole, { error: updateError, isSuccess }] =
+    useUpdateUserRoleMutation();
   const { isLoading, data, refetch } = useGetAllUsersQuery(
     {},
     { refetchOnMountOrArgChange: true }
   );
+  const [deleteUser, { isSuccess: deleteSuccess, error: deleteError }] =
+    useDeleteUserMutation({});
 
+  useEffect(() => {
+    if (updateError) {
+      if ("data" in updateError) {
+        const errorMessage = updateError as any;
+        toast.error(errorMessage.data.message);
+      }
+    }
+
+    if (isSuccess) {
+      refetch();
+      toast.success("User role updated successfully");
+      setActive(false);
+    }
+    if (deleteSuccess) {
+      refetch();
+      toast.success("Delete user successfully!");
+      setOpen(false);
+    }
+    if (deleteError) {
+      if ("data" in deleteError) {
+        const errorMessage = deleteError as any;
+        toast.error(errorMessage.data.message);
+      }
+    }
+  }, [updateError, isSuccess, deleteSuccess, deleteError]);
 
   const columns = [
     { field: "id", headerName: "ID", flex: 0.3 },
@@ -66,7 +96,7 @@ const AllUsers: FC<Props> = ({ isTeam }) => {
         return (
           <>
             <a href={`mailto:${params.row.email}`}>
-              <AiOutlineMail className="dark:text-white text-black my-4" size={20} />
+              <AiOutlineMail className="dark:text-white text-black" size={20} />
             </a>
           </>
         );
@@ -77,7 +107,8 @@ const AllUsers: FC<Props> = ({ isTeam }) => {
   const rows: any = [];
 
   if (isTeam) {
-    const newData = data && data.users.filter((item: any) => item.role === "admin");
+    const newData =
+      data && data.users.filter((item: any) => item.role === "admin");
 
     newData &&
       newData.forEach((item: any) => {
@@ -86,7 +117,7 @@ const AllUsers: FC<Props> = ({ isTeam }) => {
           name: item.name,
           email: item.email,
           role: item.role,
-          courses: item.course.length,
+          courses: item.courses.length,
           created_at: format(item.createdAt),
         });
       });
@@ -98,24 +129,37 @@ const AllUsers: FC<Props> = ({ isTeam }) => {
           name: item.name,
           email: item.email,
           role: item.role,
-          courses: item.course.length,
+          courses: item.courses.length,
           created_at: format(item.createdAt),
         });
       });
   }
+
+  const handleSubmit = async () => {
+    await updateUserRole({ email, role });
+  };
+
+  const handleDelete = async () => {
+    const id = userId;
+    await deleteUser(id);
+  };
 
   return (
     <div className="mt-[120px]">
       {isLoading ? (
         <Loader />
       ) : (
-        <Box m="0 40px 0 80px">
-          <div className="w-full flex justify-end">
-            <div className={`${styles.button} !w-[220px]`}
-                 onClick={() => setActive(!active)}>
+        <Box m="20px">
+          {isTeam && (
+            <div className="w-full flex justify-end">
+              <div
+                className={`${styles.button} !w-[200px] !rounded-[10px] dark:bg-[#57c7a3] !h-[35px] dark:border dark:border-[#ffffff6c]`}
+                onClick={() => setActive(!active)}
+              >
                 Add New Member
+              </div>
             </div>
-          </div>
+          )}
           <Box
             m="40px 0 0 0"
             height="80vh"
@@ -134,8 +178,8 @@ const AllUsers: FC<Props> = ({ isTeam }) => {
                 color: theme === "dark" ? "#fff" : "#000",
                 borderBottom:
                   theme === "dark"
-                    ? "1px solid #ffffff30 !important"
-                    : "1px solid #ccc !important",
+                    ? "1px solid #ffffff30!important"
+                    : "1px solid #ccc!important",
               },
               "& .MuiTablePagination-root": {
                 color: theme === "dark" ? "#fff" : "#000",
@@ -146,18 +190,18 @@ const AllUsers: FC<Props> = ({ isTeam }) => {
               "& .name-column--cell": {
                 color: theme === "dark" ? "#fff" : "#000",
               },
-              "& .MuiDataGrid-columnHeader": {
+              "& .MuiDataGrid-columnHeaders": {
                 backgroundColor: theme === "dark" ? "#3e4396" : "#A4A9FC",
                 borderBottom: "none",
                 color: theme === "dark" ? "#fff" : "#000",
               },
               "& .MuiDataGrid-virtualScroller": {
-                backgroundColor: theme === "dark" ? "#1F2A40 !important" : "#F2F0F0 !important",
+                backgroundColor: theme === "dark" ? "#1F2A40" : "#F2F0F0",
               },
               "& .MuiDataGrid-footerContainer": {
-                backgroundColor: theme === "dark" ? "#3e4396" : "#A4A9FC",
                 color: theme === "dark" ? "#fff" : "#000",
-                borderTop: "none"
+                borderTop: "none",
+                backgroundColor: theme === "dark" ? "#3e4396" : "#A4A9FC",
               },
               "& .MuiCheckbox-root": {
                 color:
@@ -170,6 +214,51 @@ const AllUsers: FC<Props> = ({ isTeam }) => {
           >
             <DataGrid checkboxSelection rows={rows} columns={columns} />
           </Box>
+          {active && (
+            <Modal
+              open={active}
+              onClose={() => setActive(!active)}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-[450px] bg-white dark:bg-slate-900 rounded-[8px] shadow p-4 outline-none">
+                <h1 className={`${styles.title}`}>Add New Member</h1>
+                <div className="mt-4">
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Enter email..."
+                    className={`${styles.input}`}
+                  />
+                  <select
+                    name=""
+                    id=""
+                    className={`${styles.input} !mt-6`}
+                    onChange={(e: any) => setRole(e.target.value)}
+                  >
+                    <option
+                      className="dark:bg-[#000] text-[#fff]"
+                      value="admin"
+                    >
+                      Admin
+                    </option>
+                    <option className="dark:bg-[#000] text-[#fff]" value="user">
+                      User
+                    </option>
+                  </select>
+                  <br />
+                  <div
+                    className={`${styles.button} my-6 !h-[30px]`}
+                    onClick={handleSubmit}
+                  >
+                    Submit
+                  </div>
+                </div>
+              </Box>
+            </Modal>
+          )}
+
           {open && (
             <Modal
               open={open}
@@ -179,17 +268,18 @@ const AllUsers: FC<Props> = ({ isTeam }) => {
             >
               <Box className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-[450px] bg-white dark:bg-slate-900 rounded-[8px] shadow p-4 outline-none">
                 <h1 className={`${styles.title}`}>
-                  Are you sure you want to delete this course?
+                  Are you sure you want to delete this user?
                 </h1>
                 <div className="flex w-full items-center justify-between mb-6 mt-4">
                   <div
-                    className={`${styles.button} !w-[120px] h-[30px] bg-[#47d097]`}
+                    className={`${styles.button} !w-[120px] h-[30px] bg-[#57c7a3]`}
                     onClick={() => setOpen(!open)}
                   >
                     Cancel
                   </div>
                   <div
                     className={`${styles.button} !w-[120px] h-[30px] bg-[#d63f3f]`}
+                    onClick={handleDelete}
                   >
                     Delete
                   </div>
@@ -204,4 +294,3 @@ const AllUsers: FC<Props> = ({ isTeam }) => {
 };
 
 export default AllUsers;
-
--- redux/features/user/userApi.ts
+++ redux/features/user/userApi.ts
@@ -36,7 +36,22 @@ export const userApi = apiSlice.injectEndpoints({
         credentials: "include" as const,
       }),
     }),
+    updateUserRole: builder.mutation({
+      query: ({ email, role }) => ({
+        url: "update-user",
+        method: "PUT",
+        body: { email, role },
+        credentials: "include" as const,
+      }),
+    }),
+    deleteUser: builder.mutation({
+      query: (id) => ({
+        url: `delete-user/${id}`,
+        method: "DELETE",
+        credentials: "include" as const,
+      }),
+    }),
   }),
 });
 
-export const {useUpdateAvatarMutation, useEditProfileMutation, useUpdatePasswordMutation, useGetAllUsersQuery} = userApi
+export const {useUpdateAvatarMutation, useEditProfileMutation, useUpdatePasswordMutation, useGetAllUsersQuery, useUpdateUserRoleMutation, useDeleteUserMutation} = userApi
