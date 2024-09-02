bd19131ead494e3643fbc68cf904833876691e14
Gayal Melappilly <gayalsunil@gmail.com>
2024-09-03 02:45:51 +0530

    Updated admin page with course analytics, recent transactions, Sales details and user details

--- app/components/Admin/DashboardHero.tsx
+++ app/components/Admin/DashboardHero.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import DashboardHeader from './DashboardHeader'
+import React, { useState } from "react";
+import DashboardHeader from "./DashboardHeader";
+import DashboardWidgets from "../../components/Admin/Widgets/DashboardWidgets";
 
-type Props = {}
+type Props = {
+  isDashboard?: boolean;
+};
 
-const DashboardHero = (props: Props) => {
+const DashboardHero = ({isDashboard}: Props) => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div>
-        <DashboardHeader />
+      <DashboardHeader open={open} setOpen={setOpen} />
+      {
+        isDashboard && (
+          <DashboardWidgets open={open} />
+        )
+      }
     </div>
-  )
-}
+  );
+};
 
-export default DashboardHero
+export default DashboardHero;
