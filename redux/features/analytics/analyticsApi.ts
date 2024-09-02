ec2242c9549591772536df838d6caecf453b9886
Gayal Melappilly <gayalsunil@gmail.com>
2024-09-03 02:19:26 +0530

    Added chart to orders analytics tab

--- redux/features/analytics/analyticsApi.ts
+++ redux/features/analytics/analyticsApi.ts
@@ -15,8 +15,15 @@ export const analyticsApi = apiSlice.injectEndpoints({
                 method: "GET",
                 credentials: "include" as const
             })
+        }),
+        getOrdersAnalytics: builder.query({
+            query: () => ({
+                url: "get-orders-analytics",
+                method: "GET",
+                credentials: "include" as const
+            })
         })
     })
 })
 
-export const { useGetCoursesAnalyticsQuery, useGetUsersAnalyticsQuery } = analyticsApi
+export const { useGetCoursesAnalyticsQuery, useGetUsersAnalyticsQuery, useGetOrdersAnalyticsQuery } = analyticsApi
