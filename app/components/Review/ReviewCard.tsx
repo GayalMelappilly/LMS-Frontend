b4e91d63126e5d5db52e2ae18896589bade979a5
Gayal Melappilly <gayalsunil@gmail.com>
2024-09-05 02:18:09 +0530

    Added random user profiles in review section

--- app/components/Review/ReviewCard.tsx
+++ app/components/Review/ReviewCard.tsx
@@ -10,13 +10,13 @@ const ReviewCard = (props: Props) => {
   return (
     <div className="w-full h-max pb-4  dark:bg-slate-500 dark:bg-opacity-[0.20] border border-[#00000028] dark:border-[#ffffff1d] backdrop-blur shadow-[bg-slate-700] rounded-lg p-3 shadow-inner">
       <div className="flex w-full">
-        {/* <Image
+        <Image
           src={props.item.avatar}
           alt=""
           width={50}
           height={50}
           className="w-[50px] h-[50px] rounded-full object-cover"
-        /> */}
+        />
         <div className="800px:flex justify-between w-full hidden">
           <div className="pl-4">
             <h5 className="text-[20px] text-black dark:text-white">
