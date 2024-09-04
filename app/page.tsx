6cadf574d4f07aaff442ea7c48af49a6189018b3
Gayal Melappilly <gayalsunil@gmail.com>
2024-09-05 01:55:38 +0530

    Courses section added including course cards

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import React, {FC, useState} from 'react'
 import Heading from './utils/Heading'
 import Header from './components/Header'
 import Hero from './components/Route/Hero'
+import Courses from "./components/Route/Courses";
 
 interface Props {}
 
@@ -27,6 +28,7 @@ const Page:FC<Props> = () => {
         route={route}
       />
       <Hero />
+      <Courses />
     </div>
   )
 }
