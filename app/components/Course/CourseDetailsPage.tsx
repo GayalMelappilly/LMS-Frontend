c041db2fc5779d61b6d88a8550fe6ec4286d14cc
Gayal Melappilly <gayalsunil@gmail.com>
2024-09-07 02:18:33 +0530

    Course details added

---
+++ app/components/Course/CourseDetailsPage.tsx
@@ -0,0 +1,62 @@
+import { useGetCourseDetailsQuery } from "@/redux/features/courses/coursesApi";
+import React, { useEffect, useState } from "react";
+import Loader from "../Loader/Loader";
+import Heading from "@/app/utils/Heading";
+import Header from "../Header";
+import Footer from "../Footer";
+import CourseDetails from "./CourseDetails";
+import {
+  useCreatePaymentIntentMutation,
+  useGetStripePublishablekeyQuery,
+} from "@/redux/features/orders/ordersApi";
+import { useLoadUserQuery } from "@/redux/features/api/apiSlice";
+
+type Props = {
+  id: string;
+};
+
+const CourseDetailsPage = ({ id }: Props) => {
+  const [route, setRoute] = useState("Login");
+  const [open, setOpen] = useState(false);
+  const { data, isLoading } = useGetCourseDetailsQuery(id);
+  const { data: userData } = useLoadUserQuery(undefined, {});
+  const [stripePromise, setStripePromise] = useState<any>(null);
+  const [clientSecret, setClientSecret] = useState("");
+
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <Heading
+            title={data?.course?.name + " - Learnify"}
+            description={
+              "Learnify is a programming community which is developed by Gayal Melappilly for helping programmers"
+            }
+            keywords={data?.course?.tags}
+          />
+          <Header
+            route={route}
+            setRoute={setRoute}
+            open={open}
+            setOpen={setOpen}
+            activeItem={1}
+          />
+  
+            <CourseDetails
+              data={data.course}
+              stripePromise={stripePromise}
+              clientSecret={clientSecret}
+              setRoute={setRoute}
+              setOpen={setOpen}
+            />
+        
+          <Footer />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default CourseDetailsPage;
--- redux/features/courses/coursesApi.tsx
+++ redux/features/courses/coursesApi.tsx
@@ -40,7 +40,7 @@ export const coursesApi = apiSlice.injectEndpoints({
       }),
     }),
     getCourseDetails: builder.query({
-      query: (id: any) => ({
+      query: (id) => ({
         url: `get-course/${id}`,
         method: "GET",
         credentials: "include" as const,
