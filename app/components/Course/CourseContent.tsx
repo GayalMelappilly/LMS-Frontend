b901c596a62a22c80b586a6e69b4e658305b851f
Gayal Melappilly <gayalsunil@gmail.com>
2024-09-20 00:10:18 +0530

    Aligned Video and section tab

--- app/components/Course/CourseContent.tsx
+++ app/components/Course/CourseContent.tsx
@@ -12,7 +12,7 @@ type Props = {
 };
 
 const CourseContent = ({ id, user }: Props) => {
-  const { data: contentData, isLoading,refetch } = useGetCourseContentQuery(id,{refetchOnMountOrArgChange:true});
+  const { data: contentData, isLoading, refetch } = useGetCourseContentQuery(id, { refetchOnMountOrArgChange: true });
   const [open, setOpen] = useState(false);
   const [route, setRoute] = useState('Login')
   const data = contentData?.content;
@@ -26,13 +26,13 @@ const CourseContent = ({ id, user }: Props) => {
       ) : (
         <>
           <Header activeItem={1} open={open} setOpen={setOpen} route={route} setRoute={setRoute} />
-          <div className="w-full grid 800px:grid-cols-10">
-            <Heading
-              title={data[activeVideo]?.title}
-              description="anything"
-              keywords={data[activeVideo]?.tags}
-            />
-            <div className="col-span-6">
+          <Heading
+            title={data[activeVideo]?.title}
+            description="anything"
+            keywords={data[activeVideo]?.tags}
+          />
+          <div className="w-full grid grid-cols-12">
+            <div className="col-span-8">
               <CourseContentMedia
                 data={data}
                 id={id}
@@ -42,13 +42,13 @@ const CourseContent = ({ id, user }: Props) => {
                 refetch={refetch}
               />
             </div>
-            <div className="hidden 800px:block 800px:col-span-2">
-            <CourseContentList
-              setActiveVideo={setActiveVideo}
-              data={data}
-              activeVideo={activeVideo}
-            />
-          </div>
+            <div className="hidden 800px:block 800px:col-span-4">
+              <CourseContentList
+                setActiveVideo={setActiveVideo}
+                data={data}
+                activeVideo={activeVideo}
+              />
+            </div>
           </div>
         </>
       )}
