d2347a5ad0797fde29b1f7e9593d09143cf82909
Gayal Melappilly <gayalsunil@gmail.com>
2024-10-16 01:30:57 +0530

    Wrapped useSearchParams in Suspense

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 "use client";
 import React, { FC, useEffect, useState, Suspense } from "react";
 import { useGetUsersAllCoursesQuery } from "@/redux/features/courses/coursesApi";
